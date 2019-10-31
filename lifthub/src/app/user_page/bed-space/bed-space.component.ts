35d15f574dc837f032f7c12c6c52c03adeae454c
Acexode <abudawud92@gmail.com>
2019-10-31 11:56:20 +0100

    modified bed space

--- lifthub/src/app/user_page/bed-space/bed-space.component.ts
+++ lifthub/src/app/user_page/bed-space/bed-space.component.ts
@@ -10,16 +10,39 @@ export class BedSpaceComponent implements OnInit {
     spaceType: "Bed Space",
     details : {
       name: "Single bed space",
-      img: "https://images.pexels.com/photos/1647919/pexels-photo-1647919.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      img: "https://images.pexels.com/photos/1454806/pexels-photo-1454806.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       location: "Central Business District, Abuja",
       description: "Beautifully furnished bed space with smart room gadgets",
-      price: "1,200,000",
+      price: "1,000,000",
       availability: true
-
-  
     },    
   
-  }]
+  },
+  {  
+    spaceType: "Bed Space",
+    details : {
+      name: "Cottage",
+      img: "https://images.pexels.com/photos/271743/pexels-photo-271743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      location: "IBB Way, Abuja",
+      description: "Beautifully furnished office with electronic gadgets",
+      price: "1,500,000",
+      availability: true
+    },    
+  
+  },
+  {  
+    spaceType: "Bed Space",
+    details : {
+      name: "Double bed space",
+      img: "https://images.pexels.com/photos/237371/pexels-photo-237371.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      location: "Central Business District, Abuja",
+      description: "Beautifully furnished double bed space with smart room gadgets",
+      price: "1,200,000",
+      availability: true
+    },    
+  
+  },
+]
   constructor() { }
     
   ngOnInit() {
